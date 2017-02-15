197414468ca72b54b2d14fb9097bbd0a2c3e08aa
mattgrosso <mattgrosso@gmail.com>
2017-02-15 15:53:29 -0500

    Fixed issue with inputs not clearing.

--- build/js/main.js
+++ build/js/main.js
@@ -20,17 +20,16 @@
     this.message = "";
     this.contactsList = phonebookFactory.contactsList;
 
-    this.newContact = {
-      firstName: null,
-      lastName: null,
-      phone: null
-    };
+    this.newContact = {};
 
     this.addContact = function addContact(contact) {
       var listLength = this.contactsList.length;
       phonebookFactory.addContact(contact);
+      this.newContact = {};
       if (listLength === this.contactsList.length) {
         this.message = "That contact is already included.";
+      } else {
+        this.message = "";
       }
     };
   }
